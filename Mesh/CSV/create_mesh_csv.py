e4532c45e8ccf77233a11185bce816e899c6b84b
joda2802 <johannessteppe281@gmail.com>
2025-03-17 13:37:43 +0000

    create_mesh.py added

---
+++ Mesh/CSV/create_mesh_csv.py
@@ -0,0 +1,23 @@
+import csv
+
+#Values for ice thickness and height above water level
+
+H = float(input("Enter Ice Thickness:").strip() or "42")
+hcliff = float(input("Enter Height above Water:").strip() or "42")
+L = 2000
+
+data = [
+    {'id': "1", 'xcoord': 0, 'ycoord': -(H-hcliff)},
+    {'id': "2", 'xcoord': L, 'ycoord': -(H-hcliff)},
+    {'id': "3", 'xcoord': L, 'ycoord': 0},
+    {'id': "4", 'xcoord': L, 'ycoord': hcliff},
+    {'id': "5", 'xcoord': 0, 'ycoord': hcliff},
+]
+
+with open('Mesh.csv', 'w', newline='') as csvfile:
+    fieldnames = ['id', 'xcoord', 'ycoord']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(data)
+
+
