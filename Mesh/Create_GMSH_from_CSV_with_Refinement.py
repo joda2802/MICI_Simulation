05035ac61b068cda868d80f8d816009c5b73959a
JonathanKrahl <karl.krahl@tu-dortmund.de>
2025-03-17 15:52:58 +0100

    Meshing Complete

--- Mesh/Create_GMSH_from_CSV_with_Refinement.py
+++ Mesh/Create_GMSH_from_CSV_with_Refinement.py
@@ -19,7 +19,7 @@ Schritt4: Überprüfe in Paraview ob die Tags und das Mesh sinnvoll erstellt wur
 
 '''
 
-mesh_name = "Mesh_Geometry_Brunt2_Refined"
+mesh_name = "Mesh_Initial_MICI"
 gdim = 2 #Dimension
 
 
@@ -32,9 +32,11 @@ gmsh.initialize()
 gmsh.clear()
 gmsh.model.add(mesh_name)
 
+length = len(data)
+
 #Geometrie erstellen
-points = [gmsh.model.occ.add_point(x=float(data[i][1]), y=float(data[i][2]), z=0) for i in range(1,len(data)) ]
-avg_point = gmsh.model.occ.add_point(x=np.mean([float(data[i][1]) for i in range(1,9)]), y=np.mean([float(data[i][2]) for i in range(1,9)]), z=0)
+points = [gmsh.model.occ.add_point(x=float(data[i][1]), y=float(data[i][2]), z=0) for i in range(1,length) ]
+#avg_point = gmsh.model.occ.add_point(x=np.mean([float(data[i][1]) for i in range(1,length)]), y=np.mean([float(data[i][2]) for i in range(1,length)]), z=0)
 lines = [gmsh.model.occ.add_line(points[i], points[i+1]) for i in range(0,len(points)-1)]
 lines.append(gmsh.model.occ.add_line(points[len(points)-1], points[0]))
 loop = gmsh.model.occ.add_curve_loop(lines)
@@ -49,7 +51,7 @@ gmsh.model.addPhysicalGroup(dim=gdim, tags=[area], tag=1)   #Netz
 gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(1,8), tag=2) #icerise
 gmsh.model.addPhysicalGroup(dim=gdim-1, tags=outflow, tag=3)    #outflow
 gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(43,46), tag=4)   #inflow
-'''
+
 #crevasse
 outflow =[i for i in range(8,39)]+[i for i in range(50,52)]+[i for i in range(61,71)]
 gmsh.model.addPhysicalGroup(dim=gdim, tags=[area], tag=1)   #Netz
@@ -59,7 +61,7 @@ gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(58,61), tag=4)   #inflow
 gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(39,50), tag=5)   #crack 1
 gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(52,58), tag=6)   #crack 2
 
-'''
+
 #crevasse2
 outflow =[i for i in range(8,39)]+[i for i in range(62,72)]
 
@@ -70,26 +72,39 @@ gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(59,62), tag=4)   #inflow
 gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(39,59), tag=5)   #Riss
 '''
 
-#Netz
-h_size = 1000 #Elemengröße
-h_size_fine = 200 #verfeinerte Elementgröße
-'''
-#globale Elemeltgröße
-gmsh.option.setNumber('Mesh.CharacteristicLengthFactor',h_size)
-gmsh.model.mesh.generate(dim=gdim)
-'''
+# Rectangle
+gmsh.model.addPhysicalGroup(dim=gdim, tags=[area], tag=1) # Tag for area
+gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(1,2), tag=2) # Tag for bottom
+gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(2,4), tag=3) # Tag for cliff
+gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(4,5), tag=4) # Tag for surface
+gmsh.model.addPhysicalGroup(dim=gdim-1, tags=range(5,6), tag=5) # Tag for inflow
 
+#Netz
+h_size = 50 #Elemengröße
+h_size_fine = 10 #verfeinerte Elementgröße
+
+
+
+
+
+#globale Elemeltgröße
+gmsh.option.setNumber('Mesh.CharacteristicLengthMax',h_size)
+#gmsh.model.mesh.generate(dim=gdim)
+
+"""
 #variable Elemetgröße
 fine_points = [avg_point] #Liste von Punkten, um die verfeinert werden soll
-#fine_lines = [] #Liste von Linien, um die verfeinert werden soll
-dist_min = 5000
-dist_max = 10000
+#
+"""
+fine_lines = [2,3] #Liste von Linien, um die verfeinert werden soll
+dist_min = 400
+dist_max = 600
 
 
 gmsh.model.mesh.field.add("Distance", 1)
-gmsh.model.mesh.field.setNumbers(1, "PointsList", fine_points)
-#gmsh.model.mesh.field.setNumbers(1, "CurvesList", fine_lines)
-#gmsh.model.mesh.field.setNumber(1,"Sampling",100)
+
+gmsh.model.mesh.field.setNumbers(1, "CurvesList", fine_lines)
+gmsh.model.mesh.field.setNumber(1,"Sampling",100)
 
 gmsh.model.mesh.field.add("Threshold", 2)
 gmsh.model.mesh.field.setNumber(2, "InField", 1)
@@ -97,6 +112,7 @@ gmsh.model.mesh.field.setNumber(2, "SizeMin", h_size_fine)
 gmsh.model.mesh.field.setNumber(2, "SizeMax", h_size)
 gmsh.model.mesh.field.setNumber(2, "DistMin", dist_min)
 gmsh.model.mesh.field.setNumber(2, "DistMax", dist_max)
+
 
 gmsh.model.mesh.field.setAsBackgroundMesh(2)
 gmsh.model.mesh.generate(dim=gdim)
@@ -109,3 +125,5 @@ with dlfx.io.XDMFFile(MPI.COMM_WORLD, mesh_name+'.xdmf', 'w') as xdmfout:
     xdmfout.write_mesh(mesh)
     xdmfout.write_meshtags(cell_tags, mesh.geometry) 
     xdmfout.write_meshtags(facet_tags, mesh.geometry) 
+
+# UFL conditionals
