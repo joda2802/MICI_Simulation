bc2f0df5a9e30dc07f24bcd899c3543fd4a8aafe
joda2802 <johannessteppe281@gmail.com>
2025-03-18 13:39:19 +0000

    merge

--- Mesh/CSV/create_mesh_csv.py
+++ Mesh/CSV/create_mesh_csv.py
@@ -1,17 +1,20 @@
 import csv
+#creates a 2D-trapezoid shaped mesh 
+#Input Values
 
-#Values for ice thickness and height above water level
+H = float(input("Enter Ice Thickness at the Cliff:").strip() or "1000")
+hcliff = float(input("Enter Height above Water:").strip() or "100")
+bed_slope =  float(input("Enter Slope of Bedrock:").strip() or "0")
+ice_slope =  float(input("Enter Slope of Ice Surface:").strip() or "0")
 
-H = float(input("Enter Ice Thickness:").strip() or "42")
-hcliff = float(input("Enter Height above Water:").strip() or "42")
 L = 2000
 
 data = [
-    {'id': "1", 'xcoord': 0, 'ycoord': -(H-hcliff)},
+    {'id': "1", 'xcoord': 0, 'ycoord': -(H-hcliff)-L*bed_slope},
     {'id': "2", 'xcoord': L, 'ycoord': -(H-hcliff)},
     {'id': "3", 'xcoord': L, 'ycoord': 0},
     {'id': "4", 'xcoord': L, 'ycoord': hcliff},
-    {'id': "5", 'xcoord': 0, 'ycoord': hcliff},
+    {'id': "5", 'xcoord': 0, 'ycoord': hcliff-L*ice_slope},
 ]
 
 with open('Mesh.csv', 'w', newline='') as csvfile:
--- create_mesh.sh
+++ create_mesh.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 #create rectangular mesh with refinement
-cd Mesh
-python3 CSV/create_mesh_csv.py
+cd Mesh/CSV
+
+read -p "Trapezoid-Shaped mesh? (y/n)" answer
+if [ "$answer" = "y" ];
+then
+  python3 create_mesh_csv.py
+else
+  python3 create_mesh_simple_csv.py
+fi
+
+
+cd ..
 python3 Create_GMSH_from_CSV_with_Refinement.py
