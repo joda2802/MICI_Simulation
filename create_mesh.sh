a008f2f7146b12508a1c7a18a818e4b387b80e8b
joda2802 <johannessteppe281@gmail.com>
2025-03-18 09:15:12 +0000

    fixed create_mesh.sh

--- create_mesh.sh
+++ create_mesh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #create rectangular mesh with refinement
-
-python3 Mesh/CSV/create_mesh_csv.py
-python3 Mesh/Create_GMSH_from_CSV_with_Refinement.py
+cd Mesh
+python3 CSV/create_mesh_csv.py
+python3 Create_GMSH_from_CSV_with_Refinement.py
